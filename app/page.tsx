608b8b21dd566f66245100f10a6e3835134ee8e6
briannval <brianvalentinoadhitya@gmail.com>
2024-01-31 22:59:58 -0800

    Initialized Drawer

--- app/page.tsx
+++ app/page.tsx
@@ -1,54 +1,63 @@
 "use client";
 
-import { Container, Title, Text, Button, Burger } from "@mantine/core";
+import { Container, Title, Text, Button, Burger, Drawer } from "@mantine/core";
 import classes from "./HeroImageRight.module.css";
 import { useDisclosure } from "@mantine/hooks";
+import { NavbarSimple } from "../components/NavbarSimple";
+import { ContactUs } from "../components/ContactUs";
 
 export default function Page() {
   const [opened, { toggle }] = useDisclosure();
 
   return (
-    <div className={classes.root}>
-      <Container size="lg">
-        <div className={classes.inner}>
-          <div className={classes.content}>
-            <Burger
-              opened={opened}
-              onClick={toggle}
-              color="white"
-              size={"lg"}
-              mb={10}
-            />
-            <Title className={classes.title}>
-              Building Bridges With
-              <Text
-                component="span"
-                inherit
+    <>
+      <div className={classes.root}>
+        <Container size="lg">
+          <div className={classes.inner}>
+            <div className={classes.content}>
+              <Drawer offset={10} opened={opened} onClose={toggle}>
+                <NavbarSimple />
+              </Drawer>
+              <Burger
+                opened={opened}
+                onClick={toggle}
+                color="white"
+                size={"lg"}
+                mb={10}
+              />
+              <Title className={classes.title}>
+                Building Bridges With
+                <Text
+                  component="span"
+                  inherit
+                  variant="gradient"
+                  gradient={{ from: "red", to: "yellow" }}
+                >
+                  {" "}
+                  Rwanda
+                </Text>{" "}
+              </Title>
+
+              <Text className={classes.description} mt={30}>
+                A non-profit organization designed to foster Rwanda's
+                sustainable development initiatives through practical learning
+                and action.
+              </Text>
+
+              <Button
                 variant="gradient"
                 gradient={{ from: "red", to: "yellow" }}
+                size="xl"
+                className={classes.control}
+                mt={40}
               >
-                {" "}
-                Rwanda
-              </Text>{" "}
-            </Title>
-
-            <Text className={classes.description} mt={30}>
-              A non-profit organization designed to foster Rwanda's sustainable
-              development initiatives through practical learning and action.
-            </Text>
-
-            <Button
-              variant="gradient"
-              gradient={{ from: "red", to: "yellow" }}
-              size="xl"
-              className={classes.control}
-              mt={40}
-            >
-              Donate Now
-            </Button>
+                Donate Now
+              </Button>
+            </div>
           </div>
-        </div>
-      </Container>
-    </div>
+        </Container>
+      </div>
+      <ContactUs />
+    </>
   );
 }
---
+++ components/NavbarSimple.tsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import { Group, Code, Image } from "@mantine/core";
+import {
+  IconFingerprint,
+  IconKey,
+  IconDatabaseImport,
+  IconReceipt2,
+  IconUser,
+  IconQuestionMark,
+  IconHeartHandshake,
+} from "@tabler/icons-react";
+import { MantineLogo } from "@mantinex/mantine-logo";
+import classes from "./NavbarSimple.module.css";
+
+const data = [
+  { link: "/", label: "Home", icon: IconUser },
+  { link: "", label: "Donate", icon: IconReceipt2 },
+  { link: "", label: "About", icon: IconQuestionMark },
+  { link: "", label: "Projects", icon: IconHeartHandshake },
+  { link: "", label: "Meet the Team", icon: IconUser },
+];
+
+export function NavbarSimple() {
+  const [active, setActive] = useState("Billing");
+
+  const links = data.map((item) => (
+    <a
+      className={classes.link}
+      data-active={item.label === active || undefined}
+      href={item.link}
+      key={item.label}
+      onClick={(event) => {
+        event.preventDefault();
+        setActive(item.label);
+      }}
+    >
+      <item.icon className={classes.linkIcon} stroke={1.5} />
+      <span>{item.label}</span>
+    </a>
+  ));
+
+  return (
+    <nav className={classes.navbar}>
+      <div className={classes.navbarMain}>
+        <Group className={classes.header} justify="space-between">
+          <Image
+            src="https://static.wixstatic.com/media/ec5c5c_983dfd0763e841098a6c887e9788c5f2~mv2.png/v1/fill/w_216,h_125,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/ec5c5c_983dfd0763e841098a6c887e9788c5f2~mv2.png"
+            alt="LOGO"
+            width={100}
+            height={100}
+          />
+        </Group>
+        {links}
+      </div>
+    </nav>
+  );
+}
