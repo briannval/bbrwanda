2e6a98ae73ee587d27c87435bf6bd9f4b1dcc391
briannval <brianvalentinoadhitya@gmail.com>
2024-02-16 09:26:38 -0800

    Incorporated Lama's changes

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,8 @@ import {
   Burger,
   Drawer,
   Box,
+  Group,
+  Badge,
 } from "@mantine/core";
 import classes from "./page.module.css";
 import { useDisclosure } from "@mantine/hooks";
@@ -17,6 +19,8 @@ import { FeaturesCards } from "../components/FeatureCards";
 import { FeaturesImages } from "../components/FeaturesImages";
 import { EmailBanner } from "../components/EmailBanner";
 import { VolunteerForm } from "../components/VolunteerForm";
+import { IconArrowLeftBar } from "@tabler/icons-react";
+import { IconArrowLeft } from "@tabler/icons-react";
 
 export default function Page() {
   const [opened, { toggle }] = useDisclosure();
@@ -30,13 +34,39 @@ export default function Page() {
               <Drawer offset={10} opened={opened} onClose={toggle}>
                 <NavbarSimple />
               </Drawer>
-              <Burger
-                opened={opened}
-                onClick={toggle}
-                color="white"
-                size={"lg"}
-                mb={10}
-              />
+              <Box display="flex">
+                <Group
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                  }}
+                >
+                  <Burger
+                    opened={opened}
+                    onClick={toggle}
+                    color="white"
+                    size={"lg"}
+                    mb={10}
+                  />
+                  <Badge color="orange">
+                    <Group
+                      style={{
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                      }}
+                    >
+                      <IconArrowLeft
+                        width={15}
+                        height={15}
+                        style={{ marginRight: -10 }}
+                      />
+                      Click here for more
+                    </Group>
+                  </Badge>
+                </Group>
+              </Box>
               <Title className={classes.title}>
                 Building Bridges With
                 <Text
